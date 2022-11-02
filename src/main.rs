4d50e859c2523ae43101d522805c08e258d5708f
Chema Polo <chema@informaticapolo.com>
2022-11-02 08:07:56 +0100

    Move test a unitary test

--- src/main.rs
+++ src/main.rs
@@ -28,3 +28,22 @@ fn main() {
         println!("{} x {} = {}", number, x, multiplication);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_multiplication01() {
+        let x = 2;
+        let number = 5;
+        let multiplication = x*number;
+        assert_eq!(multiplication, 10);
+    }
+
+    #[test]
+    fn test_multiplication02() {
+        let x = 3;
+        let number = 7;
+        let multiplication = x*number;
+        assert_eq!(multiplication, 21);
+    }
+}
